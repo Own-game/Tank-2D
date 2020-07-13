b79f83b9415a721f47f51fa6940f5e3713136a20
Petr <petrsmirnov1995@gmail.com>
2020-07-13 19:45:00 +0300

    Create GUI.gd

---
+++ GUI.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_tree().change_scene("res://Menu.tscn")
+	$new_game.connect("pressed", self, "btn_new_game")
+	$continue.connect("pressed", self, "btn_continue")
+	$exit.connect("pressed", self, "btn_exit")
+
+
+func btn_new_game():
+	get_tree().change_scene("res://Main.tscn")
+func btn_continue():
+	pass
+func btn_exit():
+	pass
