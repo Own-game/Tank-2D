ef3d6144b59bcdd77f03e9f02d59dc228a16b466
Petr <petrsmirnov1995@gmail.com>
2020-07-14 08:43:35 +0300

    Добавьте сообщение коммита

--- player.gd
+++ player.gd
@@ -8,8 +8,7 @@ var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	get_tree().change_scene("res://Main.tscn")
-	
+		
 func _process(delta):
 	var velocity = Vector2()
 	if Input.is_action_pressed("ui_left"):
